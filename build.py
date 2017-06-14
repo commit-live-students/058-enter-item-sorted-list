2bc166efa119883a018f69d81fda1a6ec5013af1
utk1801 <utk1801@users.noreply.github.com>
2017-06-14 13:37:14 +0000

    Done

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 import bisect
 
 def solution(list_of_nums, num):
-     bisect.insort(list_of_nums,num)
-     return sorted(list_of_nums)
+    sort=sorted(list_of_nums)
+    bisect.insort(sort,num)
+    return sort
 print solution([1, 2, 3, 5, 6], 4)
