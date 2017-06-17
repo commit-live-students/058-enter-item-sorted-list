9b6f9e31e386ec53b60c048e4dc787745ce44962
AnujaySaraf <AnujaySaraf@users.noreply.github.com>
2017-06-17 00:58:24 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,10 @@
 import bisect
 
 def solution(list_of_nums, num):
-    """Enter Code Here"""
+    """Below code is for the insertion of items in sorted order within a existing list"""
+    bisect.insort(list_of_nums, num)
+    list_of_nums.sort()
+    print list_of_nums
+    return list_of_nums
+
+solution([1,2,5,4], 3)
