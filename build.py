a4ccbce55e422952d5bf1efccbe9cf2d9cdecd6d
PankajMehar <pankaj02mehar@gmail.com>
2017-07-10 05:30:49 +0000

    Done

--- build.py
+++ build.py
@@ -1,4 +1,7 @@
 import bisect
 
 def solution(list_of_nums, num):
-    """Enter Code Here"""
+    sort_list = sorted(list_of_nums)
+    bisect.insort(sort_list,num)
+    return sort_list
+solution([1, 2, 3, 5, 6], 4)
